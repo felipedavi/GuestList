9a61178a7ee440cc73c9f0910f7c7b386baa09e4
Felipe Oliveira <31748519+felipedavi@users.noreply.github.com>
2022-04-07 13:10:41 -0300

    Singleton

--- app/src/main/java/io/github/felipedavi/guestlist/service/repository/GuestRepository.kt
+++ app/src/main/java/io/github/felipedavi/guestlist/service/repository/GuestRepository.kt
@@ -1,8 +1,21 @@
 package io.github.felipedavi.guestlist.service.repository
 
+import android.content.Context
 import io.github.felipedavi.guestlist.service.model.GuestModel
 
-class GuestRepository {
+class GuestRepository private constructor(context: Context) {
+    private var mGuestDataBaseHelper = GuestDataBaseHelper(context)
+
+    companion object {
+        private lateinit var repository: GuestRepository
+        fun getInstance(context: Context): GuestRepository {
+            if (!::repository.isInitialized) {
+                repository = GuestRepository(context)
+            }
+            return repository
+        }
+    }
+
     fun save(guest: GuestModel) {
 
     }
--- app/src/main/java/io/github/felipedavi/guestlist/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/io/github/felipedavi/guestlist/viewmodel/GuestFormViewModel.kt
@@ -1,14 +1,17 @@
 package io.github.felipedavi.guestlist.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.github.felipedavi.guestlist.service.model.GuestModel
 import io.github.felipedavi.guestlist.service.repository.GuestRepository
 
-class GuestFormViewModel: ViewModel() {
+class GuestFormViewModel(application: Application): AndroidViewModel(application) {
+    private val mContext by lazy { application.applicationContext }
+    private var mRepository = GuestRepository.getInstance(mContext)
     private var mSaveGuest = MutableLiveData<Boolean>()
-    private lateinit var mRepository: GuestRepository
     val saveGuest: LiveData<Boolean> = mSaveGuest
     fun save(name: String, presence: Boolean) {
         val guest = GuestModel(name,presence)
